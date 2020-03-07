9e1ff7e91671d52661fb485667a1d7eacca47221
Filipe Dias <fmfdias@gmail.com>
2020-03-07 01:28:56 +0000

    6: Coreui initial setup.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+// resolve-url-loader must be used before sass-loader
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader'
+});
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  'window.$': 'jquery',
+  'window.jQuery': 'jquery'
+ }));
 
 module.exports = environment
